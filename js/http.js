2529b06d6bc9d8c5526f28274a0e083cc3a5f211
張欽揚 <crazyms520@hotmail.com>
2017-07-06 23:20:30 +0800

    test

--- js/http.js
+++ js/http.js
@@ -1,12 +1,20 @@
 //http.js
 const http = require('http');
-
+var fs = require('fs');
+var file = '';
 const onRequest = (request, response) => {
     console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/Plain"});
-    response.write("Hello world");
+    response.writeHead(200, {"charset": "utf-8"});
+    // response.write("Hello world");
+    response.write(file);
     response.end();
 };
+
+fs.readFile('../index2.html', (err, data) => {
+    if (err) throw err;
+    file = data.toString();
+});
 
 http.createServer(onRequest).listen(3000);
 console.log("Serve has started");
