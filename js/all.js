7b44c8e9d0713fc0e5b6d0659f8302e928bd55e5
張欽揚 <crazyms520@hotmail.com>
2017-07-17 23:27:39 +0800

    Update 2017-07-17T15:27:34.978Z

---
+++ js/all.js
@@ -0,0 +1,2 @@
+"use strict";var newFunction=function(){};newFunction();var newFunction2=function(){};newFunction2();
+//# sourceMappingURL=all.js.map
--- js/fileSystem.js
+++
@@ -1,6 +0,0 @@
-//file system 
-var fs = require('fs');
-fs.readFile('./index2.html', (err, data) => {
-    if (err) throw err;
-    console.log(data.toString());
-})
